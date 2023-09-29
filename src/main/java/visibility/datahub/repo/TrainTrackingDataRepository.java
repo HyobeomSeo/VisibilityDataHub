48b1accdc639bc92300e25d2fedb771d7380a6f7
D-Pro360 <D-Pro360@DESKTOP-GGHD43G>
2023-09-29 15:51:58 +0900

    Init eval-no2-old-org

--- src/main/java/visibility/datahub/repo/TrainTrackingDataRepository.java
+++
@@ -1,9 +0,0 @@
-package visibility.datahub.repo;
-
-import org.springframework.data.mongodb.repository.MongoRepository;
-
-import visibility.datahub.model.rcv.train.TrainTrackingData;
-
-public interface TrainTrackingDataRepository extends MongoRepository<TrainTrackingData, String> {
-
-}
